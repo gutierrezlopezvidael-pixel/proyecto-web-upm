e5befc3fb35209efc4adfd9dd1d8f3154c45c7f0
Lexi-2a <alexia.18juarez2005@gmail.com>
2026-03-06 23:03:23 -0600

    Actualización de diseño

--- faq.js
+++ faq.js
@@ -6,3 +6,38 @@ preguntas.forEach(pregunta => {
     item.classList.toggle("activo");
   });
 });
+
+// ===== SLIDER DE BANNERS =====
+
+const slides = document.querySelectorAll(".slide");
+
+let index = 0;
+
+function cambiarSlide(){
+
+  slides[index].classList.remove("active");
+
+  index++;
+
+  if(index >= slides.length){
+    index = 0;
+  }
+
+  slides[index].classList.add("active");
+}
+
+setInterval(cambiarSlide, 4000);
+
+document.querySelectorAll(".submenu > a").forEach(menu => {
+menu.addEventListener("click", function(e){
+
+if(window.innerWidth <= 768){
+
+e.preventDefault()
+
+this.parentElement.classList.toggle("active")
+
+}
+
+})
+})
